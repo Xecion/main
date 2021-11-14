365eba14eadc2bcce8d7c7734e6ba20391aa5175
XecionFire <93848110+Xecion@users.noreply.github.com>
2021-11-14 16:35:04 +0700

    Create hayolo.js

---
+++ hayolo.js
@@ -0,0 +1,11 @@
+alert('Buat inisial "N"');
+alert('i have crush on you');
+alert('agak sakit sih waktu aku tau kamu suka sama dia');
+alert('jadii kamu nyatain aja perasaan kamu itu gimana');
+alert('kalau kamu tanya balik ke aku ga bakalan aku mengaku');
+alert('peka ya cantikk');
+alert('luvv yuu');
+
+setTimeout(()=> {
+  Location.href = 'https://wa.me/+6288803650835';
+},3000);
